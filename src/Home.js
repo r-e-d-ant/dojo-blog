59644fd554230047b397dd3883494ab7cae533f0
r-e-d-ant <mugishathierry65@gmail.com>
2022-05-12 16:11:45 +0200

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
 const Home = () => {
+    let name = 'mario';
+
+    const handleClick = () => {
+        name = 'luigi';
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
