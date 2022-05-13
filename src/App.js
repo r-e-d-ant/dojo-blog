471e17b1ed54ee3c892056df14f0ecb4afbfeb6a
r-e-d-ant <mugishathierry65@gmail.com>
2022-05-13 17:22:16 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import Navbar from './Navbar'
-import Home from './Home'
+import Navbar from './Navbar';
+import Home from './Home';
+import Create from './Create';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />}/>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
